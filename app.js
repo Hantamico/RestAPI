d9a9f55a2ec56d14ec108caab466fcb7333502a0
Kirill_Halahan <Galagan.k.a.01@gmail.com>
2023-09-04 16:20:49 +0300

    hw4

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
-require("dotenv").config();
+dotenv.config();
 
 const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
 
 const app = express();
 
@@ -14,6 +16,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
